f74a0eb18d234ba7b5b211d8584b5cbdf6ced182
Miguel Ángel <MAB015@HOTMAIL.COM>
2023-11-05 03:16:22 -0500

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        Hello World
+        {children}
+      </body>
     </html>
   );
 }
